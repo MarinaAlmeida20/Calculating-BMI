1716a656cfa4bee418a80a84c19700cb6cea5a58
Marina <marinabrito.ma@gmail.com>
2021-03-01 22:48:55 +0000

    loop, array

--- js/principal.js
+++ js/principal.js
@@ -2,36 +2,39 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes)
 
-var tdPeso = document.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = document.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = document.querySelector(".info-imc");
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if(peso <= 0 || peso >= 1000)
+for(var i = 0; i < pacientes.length; i++)
 {
-    console.log("Peso invalido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso invalido!"
-}
+    var paciente = pacientes[i];
 
-if(altura <= 0 || altura >= 3.00)
-{
-    console.log("Altura invalida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura invalida!"
-}
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-if(alturaEhValida && pesoEhValido)
-{
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso >= 1000)
+    {
+        console.log("Peso invalido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso invalido!"
+    }
+
+    if(altura <= 0 || altura >= 3.00)
+    {
+        console.log("Altura invalida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura invalida!"
+    }
+
+    if(alturaEhValida && pesoEhValido)
+    {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
