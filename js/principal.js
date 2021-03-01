7a8adfa2781b43ce72de0aa84a89dd265bf320dc
Marina <marinabrito.ma@gmail.com>
2021-03-01 22:18:13 +0000

    validation

--- js/principal.js
+++ js/principal.js
@@ -11,5 +11,24 @@ var altura = tdAltura.textContent;
 
 var tdImc = document.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso < 0 || peso > 1000)
+{
+    console.log("Peso invalido");
+    pesoEhValido = false;
+}
+
+if(altura < 0 || altura > 3.00)
+{
+    console.log("Altura invalida!");
+    alturaEhValida = false;
+}
+
+if(alturaEhValida && pesoEhValido)
+{
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
