0cadaf6aababa4ee2992069c499cacf3a4144727
Marina <marinabrito.ma@gmail.com>
2021-03-01 22:28:24 +0000

    querySelectorAll

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,8 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes)
 
 var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
